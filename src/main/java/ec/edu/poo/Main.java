fdb2aa47e58d615033db406b6af38a727eea1c9a
Laboratorio <laboratorio@udla-ec.int>
2026-05-20 12:13:16 -0500

    Primer commit

--- src/main/java/ec/edu/poo/Main.java
+++ src/main/java/ec/edu/poo/Main.java
@@ -11,11 +11,11 @@ public class Main {
         Firewall fw1 = new Firewall("FW-01", "Firewall Perimetral", 8, false, 150);
 
         System.out.println("PRUEBA DE REGISTRO");
-        System.out.println("Registrando SRV-01: " + gestor.registrarActivo(srv1));
-        System.out.println("Registrando SRV-02: " + gestor.registrarActivo(srv2));
-        System.out.println("Registrando FW-01: " + gestor.registrarActivo(fw1));
+        System.out.println("Registro SRV-01: " + gestor.registrarActivo(srv1));
+        System.out.println("Registro SRV-02: " + gestor.registrarActivo(srv2));
+        System.out.println("Registro FW-01: " + gestor.registrarActivo(fw1));
 
-        System.out.println("\n--- PRUEBA DE DUPLICADOS ---");
+        System.out.println("\n PRUEBA DE DUPLICADOS");
         Servidor srvDuplicado = new Servidor("SRV-01", "Clon", 4, false, "Linux");
         System.out.println("Registrando duplicado SRV-01: " + gestor.registrarActivo(srvDuplicado));
 
@@ -26,14 +26,14 @@ public class Main {
 
         System.out.println("Promedio de riesgo: " + gestor.calcularPromedioRiesgo());
 
-        System.out.println("\n--- PRUEBA DE PARCHES ---");
+        System.out.println("\n PRUEBA DE PARCHES");
         System.out.println("Estado parche SRV-02 antes: " + srv2.isParcheAplicado());
         System.out.println("Aplicando parche a SRV-02: " + gestor.aplicarParcheActivo("SRV-02"));
         System.out.println("Estado parche SRV-02 después: " + srv2.isParcheAplicado());
 
-        System.out.println("Aplicando parche a código inexistente (ERROR-404): " + gestor.aplicarParcheActivo("ERROR-404"));
+        System.out.println("Aplicando parche : " + gestor.aplicarParcheActivo("ERROR-404"));
 
-        System.out.println("\n--- PRUEBA DE LÍMITE DE CAPACIDAD (MAX 10) ---");
+        System.out.println("\n PRUEBA DE LÍMITE DE CAPACIDAD ");
 
         for (int i = 1; i <= 8; i++) {
             Firewall fwTest = new Firewall("TEST-" + i, "Test", 1, true, 10);
